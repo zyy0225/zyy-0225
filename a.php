be409ac02754be40ed5a5c8ef209286ff191af01
zyy0225 <1745693805@qq.com>
2018-09-28 16:31:34 +0800

    add filed

---
+++ a.php
@@ -0,0 +1,504 @@
+
+In order to be irreplaceable, one must always be different.
+
+
+要做到不可替代，就要与众不同。
+
+Every day may not be good,but there's something good in every day. 
+
+不见得每天都是好日子，但是每天总会有些好事发生的。
+
+Always go with the choice that scares you the most, because that’s the one that is going to require the most from you.
+
+就做那个让你最不安的事情，因为它能发挥你最大的潜能。
+
+The future belongs to those who believe in the beauty of their dreams.
+
+未来属于那些相信梦想之美好的人。
+
+If you wept for the missing sunset,you would miss all the shining stars.
+
+如果你为错过夕阳而哭泣，那么你就要错群星了。 
+
+We should be wakening up by the pursuit of dreams instead of the sound from an alarm. 
+
+每天叫醒你的不应该是闹钟，而应该是梦想。
+
+Don't worry too much about the ambiguous future, just make effort for explicit being present. 
+
+不为模糊不清的未来过分担忧，只为清清楚楚的现在奋发图强。
+
+Time management is really a misnomer - the challenge is not to manage time, but to manage ourselves. 
+
+“时间管理”，其实是一个误称，我们挑战的是管理自己，而不是时间。
+
+The moment you think about giving up, think of the reason why you held on so long. 
+
+每当你想放弃的时候，想一想是什么支撑着你一路坚持。
+
+Never regret. If it's good, it's wonderful. If it's bad, it's experience.
+
+不必遗憾。若是美好，叫做精彩。若是糟糕，叫做经历。
+
+Everybody wants happiness. No one wants pain. But how can you make a rainbow without a little rain?
+
+大家都想得到幸福，却不愿承担痛苦。可不经历风雨，怎能见彩虹？ 
+
+We are what we repeatedly do. Excellence, then, is not an act, but a habit. 
+
+重复的行为造就了我们；因而，卓越不是一种举动，而是一种习惯。
+
+Always be a first-rate version of yourself, instead of a second-rate version of somebody else.
+
+永远都要做最好的自己，而不是第二好的别人。
+
+There is no elevator to success. You have to take the stairs.
+
+成功没有电梯，只有一步一个脚印的楼梯。
+
+Whatever is worth doing is worth doing well.
+
+任何值得做的事就值得把它做好！
+
+Some people want it to happen, some wish it would happen, others make it happen.
+
+有些人想成功，还有些人渴望成功，另有些人通过努力使梦想成真。
+
+Don't ever let somebody tell you can't do something. You got a dream, you gotta protect it! 
+
+别让别人告诉你，你成不了才！如果你有梦想的话，就要去捍卫！
+
+Things that make life easier: A parent's hug, a lover's kiss, a friend's support.
+
+让生活变轻松有三样东西：父母的拥抱，恋人的亲吻，朋友的支持。
+
+If life is divided into two episodes, the first is “hesitance-free”, while the second is “regret-free”
+
+若将人生一分为二，前半段叫“不犹豫”，后半段叫“不后悔”。
+
+Let us face the reality, so that we are committed to our ideal. ——Che Guevara
+
+让我们面对现实，让我们忠于理想。——切·格瓦拉
+
+When it comes to going after what you love in life, don't take NO for an answer. 
+
+当你追求人生所爱时，你的字典里就不该有“不可能”这个词。
+
+A good plan today is better than a perfect plan tomorrow.--Wag the Dog 
+
+今天的好计划胜过明天的完美计划。--《摇尾狗》
+
+Every drop you drink makes your life tastier!
+
+点点滴滴，生活更美！——美汁源
+
+Nobody can go back and start a new beginning, but anyone can start today and make a new ending.
+
+没人能回头重新来过，但谁都可以从现在起书写一个不同的结局。
+
+Never interrupt your enemy when he is making a mistake. --Napoleon Bonaparte
+
+当你的敌人犯错的时候，千万不要去打扰他。--拿破伦?波拿巴
+
+Don't set your goals according to what are deemed important by others. Only you know what is best for yourself. 
+
+别人认为重要的，并不就是你的追求。只有自己才知道什么最合适。
+
+Growing in wisdom can be measured by the decrease in bitterness. 
+
+智慧的增长可用痛苦的减少来衡量。
+
+Where there is no struggle, there is no strength. -- Oprah Winfrey 
+
+没有奋斗，就没有力量。——奥普拉?温弗瑞
+
+Smile and silence are two powerful tools. Smile is the way to solve many problems and silence is the way to avoid many problems.
+
+微笑和沉默是两把利器：微笑解决很多问题，沉默避免许多问题。 
+
+Life is like riding a bicycle. To keep your balance you must keep moving.
+
+生活就像骑单车，只有不断前进，才能保持平衡。
+
+Instead of complaining that the rosebush is full of thorns, be happy that the thorn bush has roses. 
+
+与其抱怨玫瑰丛长满了刺，还不如感激刺丛里竟生出了玫瑰。
+
+Action may not always bring happiness, but there is no happiness without action. 
+
+行动不一定带来幸福，不行动就肯定没有幸福。
+
+I find that the harder I work, the more luck I seem to have. 
+
+我发现，我越是努力，就越发幸运。——托马斯·杰斐逊 
+
+Don't make promises when you're joyful. Don't reply when you're sad. Don't make decisions when you're angry. 
+
+别在喜悦时承诺，别在悲伤时回答，别在愤怒时决定。
+
+The happiest people are not those who have the best of everything, but who make the best out of everything. 
+
+最幸福的人不是拥有最好一切的人，而是把一切都变成最好的人。
+
+The life's battle is never won by the fastest or the strongest, but by the man who thinks he can.
+
+人生之战的胜利并不取决于最快或最强，而是你认为自己能不能。
+
+Do not say that opportunities never come. It did come but you just weren’t willing to give up what you had.
+
+不要说机会从未出现；它曾经来过，只是你舍不得放下已拥有的。
+
+Face the past with the least regrets, face the present with the least waste and face the future with the most dreams.
+
+不悔恨过去；不荒废现在；充满梦想，面对未来。
+
+Live as if you were to die tomorrow. Learn as if you were to live forever. -- Mahatma Gandhi
+
+如同明日将死那样生活，如同永远不死那样求知。——圣雄甘地
+
+Success is simple. Do what's right, the right way, at the right time. 
+
+成功很简单：在合适的时间用恰当的方式做正确的事。
+
+Nobody is bored when he is trying to make something that is beautiful or to discover something that is true.
+
+尝试去做一些美好的事或者去发现真理时，没有人会感到烦闷。
+
+Reading makes a full man, conference a ready man and writing an exact man.
+
+读书使人充实，讨论使人机敏，写作使人严谨。
+
+Sloth, like rust, consumes faster than labor wears.
+
+懒惰像生锈一样，比操劳更能消耗身体。
+
+Yesterday is history. Tomorrow is mystery. But today is a gift.
+
+昨日已逝，明天尚远，今天才是老天赐予的礼物。 
+
+Success is the sum of small efforts, repeated day in and day out.
+
+成功就是日复一日那一点点小小努力的积累。 
+
+A year from now, you will wish you had started today.
+
+明年今日，你会希望此时此刻自己已经行动了。
+
+Sometimes you have to stop thinking too much and just go where your heart takes you.
+
+有时候，我们不要想太多，顺着自己的心意就好了。
+
+It is our choices that show what we truly are, far more than our abilities.
+
+决定我们一生的，不是我们的能力，而是我们的选择。
+
+Some birds are not meant to be caged; their feathers are just too bright. 
+
+有些鸟儿是注定不会被关在牢笼里，它们的每一片羽毛都闪耀着光辉。
+
+Dreaming in the memory is not as good as waiting for the paradise in the hell.
+
+在回忆里继续梦幻不如在地狱里等待天堂。
+
+The turning point in the process of growing up is when you discover the core of strength within you that survives all hurt.
+
+当你从内心深处找到一种可以忍受一切痛苦的坚强力量时，你的成长历程就会出现飞跃。
+
+Sometimes you gotta shut up, swallow your pride and accept that you're wrong. It's not giving up. It's called growing up.
+
+有时候，你需要做的就是闭嘴，放下所谓的自尊，承认自己的错误。这不叫放弃，而叫成长。
+
+I'll be alright. One day. Someday. Just not today.
+
+我会好起来的。总有一天，某一天。只是不是今天。
+
+Promise yourself to be so strong that nothing can disturb your peace of mind．
+
+对自己承诺：我要强大到任何事情都无法破坏我内心的平和。
+
+Sometimes you gotta shut up, swallow your pride and accept that you're wrong. It's not giving up. It's called growing up.
+
+有时候，你需要做的就是闭嘴，放下所谓的自尊，承认自己的错误。这不叫放弃，而叫成长。
+
+I always in the deepest despair, meet the most beautiful surprise.
+
+我总在最深的绝望里，遇见最美丽的惊喜。
+
+Meeting you was fate, becoming your friend was a choice, but falling in love with you was beyond my control.
+
+遇见你是命运的安排，成为了朋友是我的选择，而爱上你是我无法控制的意外。
+
+From now on, I will expect nothing, and just take what I get.
+
+从现在起，我将不再期待，只珍惜我所拥有的。
+
+Silence is the best answer for all questions. Smiling is the best reaction in all situations.
+
+沉默是应对所有问题的最佳答案，微笑是在所有情况下的最佳反应。
+
+No one can change a person. But someone can be a reason for a person to change.
+
+没有人能改变另外一个人，但是却可能成为他改变的原因。
+
+When we were small, happiness was a simple thing. After we have grown up, simplicity is a very happy thing.
+
+小时候，幸福是件很简单的事；长大后，简单是件很幸福的事。
+
+I'm so young that everything sad or happy seems very impressive to me. A gentle touch could be a big deal.
+
+只是因为太年轻，所以所有的悲伤和快乐都显得那么深刻，轻轻一碰就惊天动
+
+Being nice to someone you dislike doesn’t mean you’re a fake. It means you’re mature enough to tolerate your dislike towards them.
+
+能够善待不太喜欢的人，并不代表你虚伪，而意味着你内心成熟到可以容纳这些不喜欢。
+
+I am a great believer in luck, and I find that the harder I work, the more I have of it. — Thomas Jefferson
+
+我非常相信运气，我发现我越努力工作，我的运气就越好。—— 托马斯?杰弗逊
+
+No one can go with you for a lifetime, so you have to adjust yourself to be alone; no one will help you again and again, so you have to fight all the time.
+
+没有人陪你走一辈子，所以你要适应孤独；没有人会帮你一辈子，所以你要一直奋斗。
+
+Getting angry is punishing oneself for mistakes of others. Forgive others, and relieve yourself.
+
+生气，就是拿别人的过错来惩罚自己。原谅别人，放过自己。
+
+Whatever is worth doing is worth doing well.
+
+任何值得做的事就值得把它做好！
+
+Trust is like a piece of paper. When it creases, it cannot recover even it is smoothed.
+
+信任就像一张纸，皱了，即使抚平，也恢复不了原样了。
+
+Whatever with the past has gone, the best is always yet to come.
+
+不管过去如何，过去的已经过去，最好的总在未来等着你。
+
+No matter how long the rain lasts, there will be a rainbow in the end. No matter how sad you may be, believe that happiness is waiting.
+
+无论下多久的雨，最后都会有彩虹；无论你多么悲伤，要相信幸福在前方等候。
+
+It's true that we don't know what we've got until we lose it, but it's also true that we don't know what we've been missing until it arrives.
+
+的确只有当我们失去时才知道曾拥有的是什么，同样，只有当我们拥有了才知道曾经失去了什么。
+
+Happiness isn't getting all you want. It's enjoying all you have. 
+
+幸福不是得到你想要的一切，而是享受你所拥有的一切。
+
+We will all have times of insecurity in life, when there is nothing to do but bravely face the feelings of doubt.
+
+生命中总有那么一段时光，充满不安，可是除了勇敢面对，我们别无选择。
+
+Friends are like four-leaf clovers. Hard to find, and lucky to have!
+
+朋友就像四叶草。寻找不易，能拥有就是幸运！
+
+It's never too late to be the person you want to be.
+
+成为自己想成为的人，永远不会太迟。
+
+Everyone's got a life that no one else knows about.
+
+各自人生，冷暖自知。
+
+Love is composed of a single soul inhabiting two bodies.
+
+爱是栖息在两个躯体里的一个灵魂。
+
+Character is what you know you are, not what others think you have.
+
+性格是你所了解的自己，而不是别人眼中的你。
+
+If you can't make a mistake, you can't make anything.
+
+如果你连犯错都不会，那么你什么也学不会。
+
+They say at the end of your life, you regret the stuff you didn't do more than the stuff that you did. —The Big Bang Theory
+
+别人都说，人到了暮年，比起自己干过的事，会更后悔没有干过一些事情。—《生活大爆炸》
+
+For anything worth having one must pay the price; and the price is always work, patience, love, self-sacrifice.—John Burroughs
+
+任何值得拥有的东西都是需要付出代价的，那就是努力、耐心、爱心以及自我牺牲。—约翰?巴勒斯
+
+In one minute you can change your attitude and in that minute you can change your entire day. —Spenser
+
+改变心态只需一分钟，而这一分钟却能改变一整天。—斯宾塞
+
+Human felicity is produced not so much by great pieces of good fortune that seldom happen, as by little advantages that occur every day.
+
+与其说人类的幸福来自偶尔发生的鸿运，不如说来自每天都有的小实惠。—富兰克林
+
+Don't be afraid of change, you may lose out on something good, but you might gain something even better.
+
+不要害怕改变，尽管你可能会因此失去一些好的东西，但你也可能会得到一些更好的东西。
+
+The reasonable man adapts himself to the world; the unreasonable one persists in trying to adapt the world to himself.
+
+明白事理的人使自己适应世界，不明事理的人想使世界适应自己。
+
+Success is not an event. It’s a journey.
+
+成功不是一个结果，而是一个过程。
+
+You make millions of decisions that mean nothing and then one day your order takes out and it changes your life. 
+
+你每天都在做很多看起来毫无意义的决定，但某天你的某个决定就能改变你的一生。—《西雅图不眠夜》
+
+Do not be picky in the social intercourse. Try to make friends with all kinds of people.
+
+交际不要挑肥拣瘦，应该和各种各样的人打成一片。
+
+Success is actually simple: when you are about to fail to hold, you just need to hang on a bit longer. 
+
+成功其实很简单，就是当你坚持不住的时候，再坚持一下。
+
+We have so many needs in our life, but ultimately, all we need is to be needed. 
+
+生命中，我们总是有那么多的渴求和需要。然而归根结底，我们最渴求的，不过是被人需要。
+
+Give everything a shot. You never know what is going to change your life. 
+
+任何事情都应该去尝试一下，因为你无法知道什么样的事将会改变你的一生。
+
+If you live to be a hundred, I want to live to be a hundred minus one day, so I never have to live without you.
+
+如果你要活到一百岁，那么我只要活到一百岁差一天，这样我就不用度过没有你陪伴的分分秒秒。——《小熊维尼》
+
+Do not rack your brains on scheming to get friendship, otherwise, it becomes sort of business trade. 
+
+友情不可费力经营，要不然就成了生意。
+
+Persistence is the common trait of anyone who has had a significant impact on the world.
+
+坚持是世界上每个有所建树的人共有的品质。
+
+Whenever you have an aim you must sacrifice something of freedom to attain it.
+
+不论什么时候，只要你有一个目标，就得牺牲一定的自由去实现它。
+
+Even if you get no applause, you should accept a curtain call gracefully and appreciate your own efforts. 
+
+即使没有人为你鼓掌，也要优雅的谢幕，感谢自己的认真付出。
+
+Whenever something negative happens to you, there is a deep lesson concealed within it.
+
+任何逆境都暗藏着让你成长的经验教训。—Eckhart Tolle
+
+If a jewel falls into the mire, it remains as precious as before; and though dust should ascend to heaven, its former worthlessness will not be altered.
+
+宝石即使落在泥潭里，仍是一样可贵；尘土纵然扬到天上，还是没有价值。
+
+The pessimist complains about the wind; the optimist expects it to change; and the realist adjusts the sails.
+
+悲观者埋怨刮风；乐观者静侯风变；现实者调整风帆。
+
+In difficult situations when hope seems feeble, the boldest plans are safest.
+
+陷于希望渺茫的困境时，最大胆的计划反而最可靠。
+
+The more time and energy you spend acquiring pertinent knowledge, the more control you have over your life. 
+
+你花更多的时间学习相关知识，就更能掌控自己人生。
+
+If a man doesn't have a strong will to get the final victory, he will never succeed in his life.
+
+一个人如果没有去赢得胜利的恒心，他必将不会在他的人生道路上取得成功。
+
+Be just to all, but trust not all.
+
+
+要公正对待所有的人，但不要轻信所有的人。
+
+Do not, for one repulse, give up the purpose that you resolved to effect.
+
+
+不要只因一次失败，就放弃你原来决心想达到的目的。
+
+The years shall run like rabbits, for in my arms I hold the flower of the ages, and the first love of the world.
+
+
+时光犹如野兔般向前飞奔，但我的手臂间还抱着古老的鲜花——还有对这世界最初的爱。
+
+Whoever saves one life, saves the world entire.
+
+
+挽救了一条生命，就等于挽救了整个世界。
+
+You never know what hand you're going to get dealt next. You learn to take life as it comes at you.
+
+
+你不会知道下一手牌会是什么，要学会接受生活。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+像我这种如狼似虎的人，若是转身，不是报恩就是报仇。
+
+
+
+
+
+
+
+
+遇到问题最大
+什么明明可以改变，怎么改变，现在怎么了
+听到了什么
+现在有什么感觉不舒服的，那些会感觉很不好
+怎样会更好，以后要怎样
+遇到问题怎么面对
+说话方式
